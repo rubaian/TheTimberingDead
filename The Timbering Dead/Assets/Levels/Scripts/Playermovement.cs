aa201f6cd3cc6c5e19a33a993dcf2a563940d0b1
fayhan <134209952+rubaian@users.noreply.github.com>
2024-11-18 01:06:35 +0300

    fixed player movement + added move sprite

--- The Timbering Dead/Assets/Levels/Scripts/Playermovement.cs
+++ The Timbering Dead/Assets/Levels/Scripts/Playermovement.cs
@@ -1,52 +1,61 @@
+using System.Numerics;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    [SerializeField]private float speed;
-    private Rigidbody2D body;
-    private bool grounded;
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpForce = 16f;
+    private bool isFacingRight = true;
 
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
 
-   private void Awake() // Awake is called when the script instance is being loaded
+   void Start()
    {
-    body = GetComponent<Rigidbody2D>();
+       
    }
 
-
-   private void Update() // Update is called once per frame
+   void Update()
    {
+       horizontal = Input.GetAxisRaw("Horizontal");
+        // Jump
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+         {
+              rb.velocity = new UnityEngine.Vector2(rb.velocity.x, jumpForce);
+         }
+        // Reduce jump force
+        if(Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new UnityEngine.Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
 
-    float horizontalInput = Input.GetAxis("Horizontal");
-    //Move
-    body.velocity= new Vector2(Input.GetAxis("Horizontal")*speed,body.velocity.y);
-
-    //Flip the player sprite
-    if(horizontalInput > 0.01f)
-    {
-        transform.localScale = new Vector3(1,1,1);
-    }
-    else if(horizontalInput < 0.01f)
-    {
-        transform.localScale = new Vector3(-1,1,1);
-    }
-    
-    //Jump
-    if(Input.GetKey(KeyCode.Space) && grounded)
-        Jump();
+       Flip();
    }
 
-   private void Jump()
+   private void FixedUpdate()
    {
-    body.velocity=new Vector2(body.velocity.x, speed);
-    grounded = false;
+    // Move the player
+    rb.velocity = new UnityEngine.Vector2(horizontal * speed, rb.velocity.y);
    }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private bool IsGrounded()
     {
-     if(collision.gameObject.tag == "Ground")
-     {
-          grounded = true;
-     }
-    }
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    } 
+
+// Flip the player
+   private void Flip()
+   {
+       if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+       {
+            isFacingRight = !isFacingRight;
+            UnityEngine.Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+       }
+   }
 }
