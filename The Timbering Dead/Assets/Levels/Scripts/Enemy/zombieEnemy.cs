cf87fab8c5ebd7bdd0e1db5e4e0e40dc2294c9d1
Alifg2005 <145504607+Alifg2005@users.noreply.github.com>
2025-02-13 00:45:45 +0300

    zombie work

---
+++ The Timbering Dead/Assets/Levels/Scripts/Enemy/zombieEnemy.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class MeleeEnemy : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private float range;
+    [SerializeField] private float colliderDistance;
+    [SerializeField] private int damge;
+    [SerializeField] private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask PlayerLayer;
+
+    private float cooldownTimer=Mathf.Infinity;
+    private Animator anim;
+    void Awake()
+    {
+        anim= GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        cooldownTimer +=Time.deltaTime;
+
+        if (PlayerInSight())
+        {
+            if (cooldownTimer >= attackCooldown)
+            {
+                cooldownTimer =0;
+                anim.SetTrigger("attack");
+            }
+        }
+    }
+
+    private bool PlayerInSight()
+    {
+    RaycastHit2D hit= 
+        Physics2D.BoxCast(boxCollider.bounds.center + transform.right*range *transform.localScale.x * colliderDistance,
+        new Vector3(boxCollider.bounds.size.x *range,boxCollider.bounds.size.y,boxCollider.bounds.size.z)
+        ,0,Vector2.left,0,PlayerLayer);
+
+    return hit.collider !=null;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right*range *transform.localScale.x *colliderDistance,
+        new Vector3(boxCollider.bounds.size.x *range,boxCollider.bounds.size.y,boxCollider.bounds.size.z));
+    }
+}
